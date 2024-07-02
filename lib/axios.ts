e6cb1516626f6a2f4aa18a4061b1bc1e1d4c07c7
wildanhkm <wildanhakim98@gmail.com>
2024-07-02 23:58:16 +0700

    update axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-const token = `Bearer ${process.env.API_ACCESS_TOKEN}`
+const token = `Bearer ${process.env.API_ACCESS_TOKEN}`;
 
 const instance = axios.create({
   baseURL: process.env.API_BASE_URL,
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache',
+    Expires: '0',
     Authorization: token,
   },
 });
